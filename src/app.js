17ca2c0a488c7c631a9d0350da063ff6e3195920
Marija <maria.katic@gmail.com>
2016-03-09 14:03:37 +0000

    added findandreplacedomtext library and function that works for multiple text matches

--- src/app.js
+++ src/app.js
@@ -3,3 +3,12 @@ annotator = require('annotator');
 $ = annotator.$
 
 //todo: write annotator extension code here
+
+var findandreplacedomtext = require('findandreplacedomtext');
+
+findandreplacedomtext(document.getElementsByClassName('test'), {
+  find: 'text',
+  replace: function(portion, match) {
+    return '[[' + portion.index + ']]';
+  }
+});
