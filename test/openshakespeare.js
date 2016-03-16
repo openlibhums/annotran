d5c86cd39236f8b2c62826c090810d0ef202d84d
Richard Brownlow <richard@dcs.bbk.ac.uk>
2016-03-16 00:52:54 +0000

    Updated for our standard development environment

--- test/openshakespeare.js
+++ test/openshakespeare.js
@@ -1,18 +1,35 @@
 OpenShakespeare = ("OpenShakespeare" in window) ? OpenShakespeare : {}
 
 OpenShakespeare.Annotator = function (element) {
-  var $ = jQuery, self = this
+    var $ = jQuery, self = this
 
-  this.annotator = $(element).annotator().data('annotator')
-  this.currentUser = null
+    this.annotator = $(element).annotator().data('annotator')
+    this.currentUser = null
 
-  this.options = {
-    user: { }
-
-
-  }
+    this.options = {
+        user: {}
 
 
+    }
+/*
+    jQuery(function ($) {
+        $('#text-to-annotate').annotator();
+    });
 
-  return this
+
+    // Setup the annotator on the page.
+    var content = $('#text-to-annotate').annotator();
+*/
+    $(element).annotator().annotator('addPlugin', 'Substitutions', 'bbk:english');
+    $(element).annotator().annotator('addPlugin', 'Tags');
+
+
+
+    jQuery(function ($) {
+       $(element).annotator()
+                .annotator('setupPlugins');
+    });
+
+
+    return this
 }
