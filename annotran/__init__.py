57b688018681dd2cbc3a3c7d005c5545956aae0d
Marija <maria.katic@gmail.com>
2016-04-28 12:16:46 +0100

    hypothesis is initialized from annotran, and template is overriden

--- annotran/__init__.py
+++ annotran/__init__.py
@@ -1,45 +1,48 @@
 from pyramid.config import Configurator
 
+from h.assets import *
+
+
+from h.config import settings_from_environment
+
+import h.app
+
+
+def includeme(config):
+    config.registry.settings.setdefault('webassets.bundles', 'annotran:assets.yaml')
+    config.include('pyramid_webassets')
+    config.override_asset(
+        to_override='h:templates/accounts/login.html.jinja2',
+        override_with='annotran:templates/accounts/login.html.jinja2')
+    config.commit()
+
+def get_settings(global_config, **settings):
+    result = {}
+    result.update(settings)
+    result.update(settings_from_environment())
+    return result
+
+
+def main(global_config, **settings):
+    settings = get_settings(global_config, **settings)
+    config = Configurator(settings=settings)
+
+    #add settings from H to init H from this annotran
+    config.set_root_factory('h.resources.create_root')
+    config.add_subscriber('h.subscribers.add_renderer_globals',
+                      'pyramid.events.BeforeRender')
+    config.add_subscriber('h.subscribers.publish_annotation_event',
+                      'h.api.events.AnnotationEvent')
+    config.add_tween('h.tweens.conditional_http_tween_factory', under='pyramid.tweens.excview_tween_factory')
+    config.add_tween('h.tweens.csrf_tween_factory')
+    config.add_tween('h.tweens.auth_token')
+    config.add_renderer('csv', 'h.renderers.CSV')
+    config.include(__name__)
+    config.include('h.app')
+    return config.make_wsgi_app()
+
+
 def configure_jinja2_assets(config):
     assets_env = config.get_webassets_env()
     jinja2_env = config.get_jinja2_environment()
     jinja2_env.assets_environment = assets_env
-
-
-def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
-
-
-    config = Configurator(settings=settings)
-    config.include('pyramid_jinja2')
-    config.add_route('app.html', '/')
-    config.add_route('hello', '/howdy')
-    config.scan('.views')
-    return config.make_wsgi_app()
-
-    '''
-    config = Configurator(settings=settings)
-    config.include('pyramid_chameleon')
-    config.include('pyramid_chameleon')
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.scan()
-    return config.make_wsgi_app()
-
-    config = Configurator(settings=settings)
-    config.include('pyramid_jinja2')
-    config.add_jinja2_extension('h.jinja_extensions.Filters')
-    config.add_jinja2_extension('h.jinja_extensions.IncludeRawExtension')
-    config.add_jinja2_extension('webassets.ext.jinja2.AssetsExtension')
-    config.action(None, configure_jinja2_assets, args=(config,))
-
-    config.add_route('home', '/')
-    config.scan()
-    return config.make_wsgi_app()
-
-    '''
-
-
-  #  config.add_route('hello', '/howdy')
-   # config.scan('.views')
