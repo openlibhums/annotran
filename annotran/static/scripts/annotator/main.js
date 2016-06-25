dea0a90bb8fcd2b78d125b4ec9c38b7bbc80e706
Martin Eve <martin@martineve.com>
2016-06-25 17:01:41 +0100

    Modify structure to compile in new plugins to annotator. Now working.
    (Sentence tagging not yet working.)

--- annotran/static/scripts/annotator/main.js
+++ annotran/static/scripts/annotator/main.js
@@ -1,5 +1,57 @@
 var extend = require('extend');
 var Annotator = require('annotator');
 
+// Polyfills
+var g = Annotator.Util.getGlobal();
+if (g.wgxpath) {
+  g.wgxpath.install();
+}
+
+// Applications
+Annotator.Guest = require('../../../../../h/h/static/scripts/annotator/guest');
+Annotator.Host = require('../../../../../h/h/static/scripts/annotator/host');
+Annotator.Sidebar = require('./sidebar');
+Annotator.PdfSidebar = require('../../../../../h/h/static/scripts/annotator/pdf-sidebar');
+
+// UI plugins
+Annotator.Plugin.BucketBar = require('../../../../../h/h/static/scripts/annotator/plugin/bucket-bar');
+Annotator.Plugin.Toolbar = require('../../../../../h/h/static/scripts/annotator/plugin/toolbar');
+
+// Document type plugins
+Annotator.Plugin.PDF = require('../../../../../h/h/static/scripts/annotator/plugin/pdf');
+require('../../../../../h/h/static/scripts/vendor/annotator.document');  // Does not export the plugin :(
+
 // Selection plugins
+Annotator.Plugin.TextSelection = require('../../../../../h/h/static/scripts/annotator/plugin/textselection');
 Annotator.Plugin.SentenceSelection = require('./plugin/sentenceselection');
+
+// Cross-frame communication
+Annotator.Plugin.CrossFrame = require('../../../../../h/h/static/scripts/annotator/plugin/cross-frame');
+Annotator.Plugin.CrossFrame.AnnotationSync = require('../../../../../h/h/static/scripts/annotation-sync');
+Annotator.Plugin.CrossFrame.Bridge = require('../../../../../h/h/static/scripts/bridge');
+Annotator.Plugin.CrossFrame.Discovery = require('../../../../../h/h/static/scripts/discovery');
+
+var docs = 'https://h.readthedocs.org/en/latest/hacking/customized-embedding.html';
+var options = {
+  app: jQuery('link[type="application/annotator+html"]').attr('href')
+};
+
+if (window.hasOwnProperty('hypothesisConfig')) {
+  if (typeof window.hypothesisConfig === 'function') {
+    extend(options, window.hypothesisConfig());
+  } else {
+    throw new TypeError('hypothesisConfig must be a function, see: ' + docs);
+  }
+}
+
+Annotator.noConflict().$.noConflict(true)(function() {
+  'use strict';
+  var Klass = window.PDFViewerApplication ?
+      Annotator.PdfSidebar :
+      Annotator.Sidebar;
+  if (options.hasOwnProperty('constructor')) {
+    Klass = options.constructor;
+    delete options.constructor;
+  }
+  window.annotator = new Klass(document.body, options);
+});
--- annotran/static/scripts/annotator/plugin/sentenceselection.coffee
+++ annotran/static/scripts/annotator/plugin/sentenceselection.coffee
@@ -6,42 +6,42 @@ $ = Annotator.$
 module.exports = class SentenceSelection extends Annotator.Plugin
 
   pluginInit: ->
-# Register the event handlers required for creating a selection
-  $(document).bind({
-    "click": @makeSentenceSelection
-  })
+    # Register the event handlers required for creating a selection
+    $(document).bind({
+      "click": @makeSentenceSelection
+    })
 
-  null
+    null
 
-destroy: ->
-  $(document).unbind({
-    "click": @makeSentenceSelection
-  })
-  super
+  destroy: ->
+    $(document).unbind({
+      "click": @makeSentenceSelection
+    })
+    super
 
-# This is called when the mouse is clicked on a DOM element.
-# Checks to see if there is a sentence that we can select, if so
-# calls Annotator's onSuccessfulSelection method.
-#
-# event - The event triggered this. Usually it's a click Event
-#
-# Returns nothing.
-makeSentenceSelection: (event = {}) =>
-# Get the currently selected ranges.
-  range = document.createRange()
-  range.selectNodeContents(event.target)
+  # This is called when the mouse is clicked on a DOM element.
+  # Checks to see if there is a sentence that we can select, if so
+  # calls Annotator's onSuccessfulSelection method.
+  #
+  # event - The event triggered this. Usually it's a click Event
+  #
+  # Returns nothing.
+  makeSentenceSelection: (event = {}) =>
+    # Get the currently selected ranges.
+    range = document.createRange()
+    range.selectNodeContents(event.target)
 
-  desiredText = event.target.innerText || event.target.textContent
-  desiredText = desiredText.split('.')[0]
+    desiredText = event.target.innerText || event.target.textContent
+    desiredText = desiredText.split('.')[0]
 
-  currentText = range.toString()
+    currentText = range.toString()
 
-  if desiredText != currentText
-#range.collapse (true)
-#range.setEnd(event.target, 5)
-    alert(range.endOffset)
-    alert(range.toString())
+    if desiredText != currentText
+      #range.collapse (true)
+      #range.setEnd(event.target, 5)
+      alert(range.endOffset)
+      alert(range.toString())
 
 
-  window.getSelection().removeAllRanges()
-  window.getSelection().addRange(range)
+    window.getSelection().removeAllRanges()
+    window.getSelection().addRange(range)
