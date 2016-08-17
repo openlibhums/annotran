2c64d1a3013e6c7a72742663e84206abc1428b39
Marija <maria.katic@gmail.com>
2016-08-17 15:47:50 +0100

    unneeded and duplicated code removed

--- annotran/static/scripts/pages.js
+++ annotran/static/scripts/pages.js
@@ -8,25 +8,10 @@ var eventsa =  require('./events');
 // @ngInject
 function pages(settings, session, $rootScope, $http) {
 
-    var pageId = $rootScope.pageid;
-    pageId = decodeURIComponent(pageId);
-    pageId = encodeURIComponent(encodeURIComponent(getParameterByName("url", pageId)));
-
-    function getParameterByName(name, url) {
-        if (!url) url = pageid;
-        name = name.replace(/[\[\]]/g, "\\$&");
-        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-            results = regex.exec(url);
-        if (!results) return null;
-        if (!results[2]) return '';
-        return results[2];
-        //return decodeURIComponent(results[2].replace(/\+/g, " "));
-    }
-
     function addPage(languageName) {
         var response = $http({
             method: 'POST',
-            url: settings.serviceUrl + 'pages/' + languageName + '/' + pageId + '/' + $rootScope.groupPubid + '/addPage',
+            url: settings.serviceUrl + 'pages/' + languageName + '/' +  $rootScope.pageid  + '/' + $rootScope.groupPubid + '/addPage',
         });
         return response;
     };
