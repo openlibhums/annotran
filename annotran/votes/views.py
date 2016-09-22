51d9b2c4f5863e2f66e7befaf2bbef354fb23dec
Martin Eve <martin@martineve.com>
2016-09-22 11:54:06 +0100

    Cleanup and documentation on votes views

--- annotran/votes/views.py
+++ annotran/votes/views.py
@@ -1,22 +1,26 @@
 import urllib
 
+import annotran
+import annotran.languages.models
+import annotran.pages.models
+import h
+import h.groups.models
+import h.models
+import models
+from h import i18n
 from pyramid import httpexceptions as exc
 from pyramid.view import view_config
-from h import i18n
-from annotran.util import util
-
-import models
-import annotran
-import urllib
-import h
-
 
 _ = i18n.TranslationString
 
 
-@view_config(route_name='vote_add',
-             request_method='POST')
-def addVote(request):
+@view_config(route_name='vote_add', request_method='POST')
+def add_vote(request):
+    """
+    Add a vote to the database
+    :param request: the current request object
+    :return: a redirect to language read
+    """
     if request.authenticated_userid is None:
         raise exc.HTTPNotFound()
 
@@ -24,26 +28,25 @@ def addVote(request):
     if voter is None:
         raise exc.HTTPNotFound()
 
-    languageId = request.matchdict["languageId"]
-    pageId = request.matchdict["pageId"]
+    page_url = urllib.unquote(urllib.unquote(request.matchdict["page_id"]))
+    public_group_id = request.matchdict['public_group_id']
+    public_language_id = request.matchdict["public_language_id"]
     score = request.matchdict["score"]
-    userId = request.matchdict['userId']
-    groupPubid = request.matchdict['groupId']
+    user_id = request.matchdict['user_id']
 
-    pageId = urllib.unquote(urllib.unquote(pageId))
-    page = annotran.pages.models.Page.get_by_uri(pageId)
-    language = annotran.languages.models.Language.get_by_public_language_id(languageId, page)
-    author = h.models.User.get_by_username(userId)
+    author = h.models.User.get_by_username(user_id)
+    group = h.groups.models.Group.get_by_pubid(public_group_id)
+    page = annotran.pages.models.Page.get_by_uri(page_url)
     voter = h.models.User.get_by_username(request.authenticated_user.username)
-    group = h.groups.models.Group.get_by_pubid(groupPubid)
 
+    language = annotran.languages.models.Language.get_by_public_language_id(public_language_id, page)
 
     if language is None or page is None:
         raise exc.HTTPNotFound()
 
     vote = models.Vote.get_vote(page, language, group, author, voter)
 
-    #storing last selected value only
+    # storing last selected value only
     if vote:
         request.db.delete(vote)
         request.db.flush()
@@ -51,46 +54,39 @@ def addVote(request):
     request.db.add(vote)
     request.db.flush()
 
-    #url = request.route_url('vote_read', userid=userId, languageid=languageId, pageid=request.matchdict["pageId"])
-    url = request.route_url('language_read', public_language_id=language.pubid, public_group_id=groupPubid)
+    url = request.route_url('language_read', public_language_id=language.pubid, public_group_id=public_group_id)
     return exc.HTTPSeeOther(url)
 
-@view_config(route_name='vote_delete',
-             request_method='POST')
-def deleteVote(request):
+
+@view_config(route_name='vote_delete', request_method='POST')
+def delete_vote(request):
+    """
+    Delete a vote from the database
+    :param request: the current request object
+    :return: a redirect to language read
+    """
     if request.authenticated_userid is None:
         raise exc.HTTPNotFound()
 
-    languageId = request.matchdict["languageId"]
-    groupPubid = request.matchdict['groupId']
-    pageId = urllib.unquote(urllib.unquote(request.matchdict['pageId']))
+    public_language_id = request.matchdict["public_language_id"]
+    public_group_id = request.matchdict['public_group_id']
+    page_url = urllib.unquote(urllib.unquote(request.matchdict['page_id']))
 
-    page = annotran.pages.models.Page.get_by_uri(pageId)
-    language = annotran.languages.models.Language.get_by_public_language_id(languageId, page)
-    user = h.models.User.get_by_username(request.authenticated_user.username) #only authenticated used can delete translations and consequently their scores
-    group = h.groups.models.Group.get_by_pubid(groupPubid)
+    page = annotran.pages.models.Page.get_by_uri(page_url)
+    language = annotran.languages.models.Language.get_by_public_language_id(public_language_id, page)
 
-    models.Vote.delete_votes( page, language, group, user)
+    # only authenticated used can delete translations and consequently their scores
+    user = h.models.User.get_by_username(request.authenticated_user.username)
+    group = h.groups.models.Group.get_by_pubid(public_group_id)
+
+    models.Vote.delete_votes(page, language, group, user)
     request.db.flush()
 
-    url = request.route_url('language_read', pubid=language.pubid, groupubid=groupPubid)
+    url = request.route_url('language_read', pubid=language.pubid, groupubid=public_group_id)
     return exc.HTTPSeeOther(url)
 
-@view_config(route_name='vote_read', request_method='GET')
-def read(request):
-    url=util.get_url_from_request(request)
-    languageId = request.matchdict["languageId"]
-    page = annotran.pages.models.Page.get_by_uri(url)
-    language = annotran.languages.models.Language.get_by_public_language_id(languageId, page)
-    if not request.authenticated_userid:
-        return None
-
-    models.Vote.get_votes_for_authors(page, language)
-
-    return None
 
 def includeme(config):
-    config.add_route('vote_add', 'votes/{userId}/{groupId}/{languageId}/{pageId}/{score}/addVote')
-    config.add_route('vote_delete', 'votes/{groupId}/{languageId}/{pageId}/deleteVote')
-    config.add_route('vote_read', '/votes/{userid}/{languageid}/{pageid}')
+    config.add_route('vote_add', 'votes/{user_id}/{public_group_id}/{public_language_id}/{page_id}/{score}/addVote')
+    config.add_route('vote_delete', 'votes/{public_group_id}/{public_language_id}/{page_id}/deleteVote')
     config.scan(__name__)
