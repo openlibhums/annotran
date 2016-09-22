a33fdcc931fe33741d0ba63a790c4de33c6d4dcc
Martin Eve <martin@martineve.com>
2016-09-22 10:57:28 +0100

    Cleanup, refactoring and documentation for reports models

--- annotran/reports/models.py
+++ annotran/reports/models.py
@@ -1,36 +1,47 @@
 # -*- coding: utf-8 -*-
 
 
-import annotran.pages.models
 import annotran.languages.models
-import h.groups.models
+import annotran.pages.models
 import h.accounts.models
+import h.groups.models
 import sqlalchemy as sa
 from h.db import Base
 from sqlalchemy.orm import exc
 
 
 class Report(Base):
+    """
+    Represents an abuse report in the database
+    """
     __tablename__ = 'report'
 
     id = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
 
     page_id = sa.Column(sa.Integer, sa.ForeignKey(annotran.pages.models.Page.id))
-    page = sa.orm.relationship('Page', backref='preport')
+    page = sa.orm.relationship('Page', backref='page_report')
 
     language_id = sa.Column(sa.Integer, sa.ForeignKey(annotran.languages.models.Language.id))
-    language = sa.orm.relationship('Language', backref='lreport')
+    language = sa.orm.relationship('Language', backref='language_report')
 
     group_id = sa.Column(sa.Integer, sa.ForeignKey(h.groups.models.Group.id), nullable=True)
-    group = sa.orm.relationship('Group', backref='greport')
+    group = sa.orm.relationship('Group', backref='group_report')
 
     author_id = sa.Column(sa.Integer, sa.ForeignKey(h.accounts.models.User.id))
-    author = sa.orm.relationship('User', backref='areport', foreign_keys=[author_id])
+    author = sa.orm.relationship('User', backref='author_report', foreign_keys=[author_id])
 
     reporter_id = sa.Column(sa.Integer, sa.ForeignKey(h.accounts.models.User.id))
-    Reporter = sa.orm.relationship('User', backref='vreport', foreign_keys=[reporter_id])
+    Reporter = sa.orm.relationship('User', backref='reporter_report', foreign_keys=[reporter_id])
 
     def __init__(self, page, language, group, author, reporter):
+        """
+        Initialize a report
+        :param page: the page to which the report corresponds
+        :param language: the language to which the report corresponds
+        :param group: the group to which the report corresponds
+        :param author: the translation author to which the report corresponds
+        :param reporter: the reporting user to which the report corresponds
+        """
         if page and language and author and reporter:
             self.page_id = page.id
             self.language_id = language.id
@@ -42,6 +53,15 @@ class Report(Base):
 
     @classmethod
     def get_report(cls, page, language, group, author, reporter):
+        """
+        Get a report by page, language, group, author, and reporter
+        :param page: the page to query
+        :param language: the language to query
+        :param group: the group to query or None
+        :param author: the author to query
+        :param reporter: the reporting user to query
+        :return:
+        """
         try:
             if group is None:
                 return cls.query.filter(
@@ -62,15 +82,21 @@ class Report(Base):
 
     @classmethod
     def get_by_id(cls, id_):
-        """Return the report with the given id, or None."""
+        """
+        Get a report with a specific ID
+        :param id_: the ID to query
+        :return: a report or None
+        """
         try:
             return cls.query.filter(
                 cls.id == id_).one()
         except exc.NoResultFound:
             return None
 
-
     @classmethod
     def get_all(cls):
-        """Return all reports."""
+        """
+        Get all reports
+        :return: a list of all reports or an empty list
+        """
         return cls.query.all()
