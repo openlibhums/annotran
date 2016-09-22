12ddda6f2e405821a639f7fc46a6254dd698ef93
Martin Eve <martin@martineve.com>
2016-09-22 11:02:25 +0100

    Cleanup, refactoring and documentation for reports views

--- annotran/reports/views.py
+++ annotran/reports/views.py
@@ -1,23 +1,24 @@
 import urllib
-
-from pyramid import httpexceptions as exc
-from pyramid.view import view_config
-from h import i18n
-from annotran.util import util
+import urllib
 
 import annotran
-import urllib
-import h
-
-import annotran.pages.models
 import annotran.languages.models
-import h.models
-import h.groups.models
+import annotran.pages.models
 import annotran.reports.models
+import h
+import h.groups.models
+import h.models
+from pyramid import httpexceptions as exc
+from pyramid.view import view_config
 
 
 @view_config(route_name='report_add', request_method='POST')
-def addReport(request):
+def add_report(request):
+    """
+    Add an abuse report to the database
+    :param request: a request object
+    :return: a redirect to the abuse reports page
+    """
     if request.authenticated_userid is None:
         raise exc.HTTPNotFound()
 
@@ -25,25 +26,23 @@ def addReport(request):
     if reporter is None:
         raise exc.HTTPNotFound()
 
-    languageId = request.matchdict["languageId"]
-    pageId = request.matchdict["pageId"]
-    userId = request.matchdict['userId']
-    groupPubid = request.matchdict['groupId']
+    public_language_id = request.matchdict["public_language_id"]
+    page_url = urllib.unquote(urllib.unquote(request.matchdict["page_uri"]))
+    public_group_id = request.matchdict['public_group_id']
+    user_id = request.matchdict['user_id']
 
-    pageId = urllib.unquote(urllib.unquote(pageId))
-    page = annotran.pages.models.Page.get_by_uri(pageId)
-    language = annotran.languages.models.Language.get_by_public_language_id(languageId, page)
-    author = h.models.User.get_by_username(userId)
+    page = annotran.pages.models.Page.get_by_uri(page_url)
+    language = annotran.languages.models.Language.get_by_public_language_id(public_language_id, page)
+    author = h.models.User.get_by_username(user_id)
     reporter = h.models.User.get_by_username(request.authenticated_user.username)
-    group = h.groups.models.Group.get_by_pubid(groupPubid)
-
+    group = h.groups.models.Group.get_by_pubid(public_group_id)
 
     if language is None or page is None:
         raise exc.HTTPNotFound()
 
     report = annotran.reports.models.Report.get_report(page, language, group, author, reporter)
 
-    #storing last selected value only
+    # storing last selected value only
     if report:
         request.db.delete(report)
         request.db.flush()
@@ -56,5 +55,10 @@ def addReport(request):
 
 
 def includeme(config):
-    config.add_route('report_add', 'reports/{userId}/{groupId}/{languageId}/{pageId}/addReport')
+    """
+    Pyramid's router configuration
+    :param config: the configuration object to which to push our routes
+    :return: None
+    """
+    config.add_route('report_add', 'reports/{user_id}/{public_group_id}/{public_language_id}/{page_uri}/addReport')
     config.scan(__name__)
