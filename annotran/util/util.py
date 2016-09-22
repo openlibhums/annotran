f3c5527e5d0be8e303f249cd6c8c03f9f97478f0
Martin Eve <martin@martineve.com>
2016-09-22 11:17:59 +0100

    Cleanup and documentation on util.util

--- annotran/util/util.py
+++ annotran/util/util.py
@@ -1,18 +1,30 @@
-import urllib
 import re
+import urllib
 
 
 def strip_logout(url):
+    """
+    Removes the in-built hypothes.is-appended &__formid__=logout from a URL
+    :param url: the URL
+    :return: a clean URL
+    """
     try:
-        url = re.sub('\&__formid__=logout$', '', url).split("#")[0]
-    except:
+        url = re.sub('&__formid__=logout$', '', url).split("#")[0]
+    except IndexError:
         url = ''
     return url
 
+
 def get_url_from_request(request):
+    """
+    Extracts a URL from a request and removes the in-built hypothes.is-appended &__formid__=login
+    :param request: the request
+    :return: a clean URL
+    """
     try:
-        url = urllib.unquote(urllib.unquote(request.url.split('?')[1].replace('url=', '')).split('?')[1].replace('url=', ''))
-        url = re.sub('\&__formid__=login$', '', url).split("#")[0]
-    except:
+        url = urllib.unquote(
+            urllib.unquote(request.url.split('?')[1].replace('url=', '')).split('?')[1].replace('url=', ''))
+        url = re.sub('&__formid__=login$', '', url).split("#")[0]
+    except IndexError:
         url = ''
     return url
