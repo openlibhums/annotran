6ca5a5182aabf76c500b6efae98e12731e57033e
mari-ja <maria.katic@gmail.com>
2016-10-19 14:15:10 +0100

    Update tests for reports view.

--- annotran/reports/test/views_test.py
+++ annotran/reports/test/views_test.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 import mock
-import pytest
-from pyramid import httpexceptions as exc
 
+from pyramid import httpexceptions as exc
+from mock import PropertyMock
+from mock import MagicMock
 from annotran.reports import views
 from annotran.admin import *
 
 _SENTINEL = object()
-
 
 def _mock_request(feature=None, settings=None, params=None,
                   authenticated_userid=_SENTINEL, route_url=None, **kwargs):
     """Return a mock Pyramid request object."""
     params = params or {"foo": "bar"}
     if authenticated_userid is _SENTINEL:
-        authenticated_userid = "acct:fred@hypothes.is"
+        authenticated_userid = "acct:fred@annotran.com"
     return mock.Mock(
         feature=feature or (lambda feature: True),
         registry=mock.Mock(settings=settings or {}),
@@ -23,133 +23,116 @@ def _mock_request(feature=None, settings=None, params=None,
         route_url=route_url or mock.Mock(return_value="test-read-url"),
         **kwargs)
 
-# The fixtures required to mock all of create()'s dependencies.
-create_fixtures = pytest.mark.usefixtures('Report',
-                                          'session_model')
 
+def test_add_report_to_db():
+    """
+        This should add a new report to the database session.
+        After successfully creating a new report it should redirect.
+    """
+    with mock.patch('annotran.languages.models.Language') as language:
+        language.get_by_public_language_id = MagicMock(return_value=language)
 
-@create_fixtures
-def test_create_adds_new_report_to_db(Group, Language, Page, User):
-    """This should add the new report to the database session."""
-    author = mock.Mock(id=1, username="test", uid="test")
-    User.return_value = author
+        with mock.patch('annotran.pages.models.Page') as page:
+            page.get_by_uri = MagicMock(return_value=page)
 
-    group = mock.Mock(id=1)
-    Group.return_value = group
+            with mock.patch('h.groups.models.Group') as group:
+                group.get_by_pubid = MagicMock(return_value=group)
 
-    page = mock.Mock(id=1, uri='http://www.annotran_test.com')
-    Page.return_value = page
+                with mock.patch('h.models.User') as user:
+                    propUser = PropertyMock(return_value=2897)
+                    type(user).id = propUser
+                    user.get_by_username = MagicMock(return_value=user)
 
-    language = mock.Mock(id=1)
-    Language.return_value = language
+                    with mock.patch('annotran.translations.models.Translation') as translation:
+                        propPage = PropertyMock(return_value=2897)
+                        type(translation).page_id = propPage
 
-    request = _mock_request(matchdict={'page_uri': page.uri,
-                                       'public_group_id': group.pubid,
-                                       'public_language_id': language.pubid,
-                                       'score': 5,
-                                       'user_id': author.username},
-                            authenticated_user=mock.Mock(id=2, username="test2", uid="test2"))
+                        propLang = PropertyMock(return_value=2897)
+                        type(translation).language_id = propLang
 
-    delete_report(page, language, group, author)
-    assert views.add_report(request).status_code == 200
+                        propGroup = PropertyMock(return_value=2897)
+                        type(translation).group_id = propGroup
 
+                        translation.get_translation = MagicMock(return_value=translation)
 
-@create_fixtures
-def test_create_adds__existing_report_to_db(Report, Group, Language, Page, User):
-    author = mock.Mock(id=1, username="test", uid="test")
-    User.return_value = author
+                        request = _mock_request(authenticated_user=mock.Mock(username="test"),
+                                                matchdict={'public_language_id': '12345',
+                                                           'public_group_id': '12345',
+                                                           'user_id': '12345',
+                                                           'page_uri': 'http://www.annotran_test.com/'})
+                        result = views.add_report(request)
+                        request.db.add.assert_called_once()
+                        assert result == {}
 
-    report = mock.Mock(id=1)
-    Report.return_value = report
-
-    group = mock.Mock(id=1)
-    Group.return_value = group
-
-    page = mock.Mock(id=1, uri='http://www.annotran_test.com')
-    Page.return_value = page
-
-    language = mock.Mock(id=1)
-    Language.return_value = language
-
-    request = _mock_request(matchdict={'page_uri': page.uri,
-                                       'public_group_id': group.pubid,
-                                       'public_language_id': language.pubid,
-                                       'score': 5,
-                                       'user_id': author.username},
-                            authenticated_user=mock.Mock(id=2, username="test2", uid="test2"))
-
-    delete_report(page, language, group, author)
-
-    assert views.add_report(request).status_code == 400
-
-
-'''
-@create_fixtures
-def test_create_redirects_to_report_read_report(Report, Group, Language, Page, User):
-    """After successfully creating a new report it should redirect."""
-    author = mock.Mock(id=1, username="test", uid="test")
-    User.return_value = author
-
-    report = mock.Mock(id=1)
-    Report.return_value = report
-
-    group = mock.Mock(id=1)
-    Group.return_value = group
-
-    page = mock.Mock(id=1, uri='http://www.annotran_test.com')
-    Page.return_value = page
-
-    language = mock.Mock(id=1)
-    Language.return_value = language
-
-    request = _mock_request(matchdict={'page_id': page.uri,
-                                       'public_group_id': group.pubid,
-                                       'public_language_id': language.pubid,
-                                       'score': 5,
-                                       'username': author.username},
-                            authenticated_user=mock.Mock(id=2, username="test2", uid="test2"))
-
+def test_add_report_to_db_pg_auth_rep_group_lang_none():
+    """
+        This should return HTTPNotFound as all page, author, reporter, group, and language is None.
+    """
+    request = _mock_request(authenticated_user=mock.Mock(username="test"),
+                            matchdict={'public_language_id': '12345',
+                                       'public_group_id': '12345',
+                                       'user_id': '12345',
+                                       'page_uri': 'http://www.annotran_test.com/'})
     result = views.add_report(request)
-
-    assert isinstance(result, httpexceptions.HTTPRedirection)
-'''
+    assert isinstance(result, exc.HTTPNotFound)
 
 
-@pytest.fixture
-def Report(request):
-    patcher = mock.patch('annotran.reports.models.Report', autospec=True)
-    request.addfinalizer(patcher.stop)
-    return patcher.start()
+def test_add_report_to_db_translation_none():
+    """
+        This should return HTTPNotFound as translation is None.
+    """
+    with mock.patch('annotran.languages.models.Language') as language:
+        language.get_by_public_language_id = MagicMock(return_value=language)
 
-@pytest.fixture
-def Page(request):
-    patcher = mock.patch('annotran.pages.models.Page', autospec=True)
-    request.addfinalizer(patcher.stop)
-    return patcher.start()
+        with mock.patch('annotran.pages.models.Page') as page:
+            page.get_by_uri = MagicMock(return_value=page)
 
-@pytest.fixture
-def Language(request):
-    patcher = mock.patch('annotran.languages.models.Language', autospec=True)
-    request.addfinalizer(patcher.stop)
-    return patcher.start()
+            with mock.patch('h.groups.models.Group') as group:
+                group.get_by_pubid = MagicMock(return_value=group)
 
-@pytest.fixture
-def Group(request):
-    patcher = mock.patch('h.groups.models.Group', autospec=True)
-    request.addfinalizer(patcher.stop)
-    return patcher.start()
+                with mock.patch('h.models.User') as user:
+                    user.get_by_username = MagicMock(return_value=user)
 
-@pytest.fixture
-def User(request):
-    patcher = mock.patch('h.accounts.models.User', autospec=True)
-    request.addfinalizer(patcher.stop)
-    return patcher.start()
+                    with mock.patch('annotran.translations.models.Translation') as translation:
+                        translation.get_translation = MagicMock(return_value=None)
 
-@pytest.fixture
-def session_model(request):
-    patcher = mock.patch('annotran.session.model')
-    request.addfinalizer(patcher.stop)
-    return patcher.start()
+                        request = _mock_request(authenticated_user=mock.Mock(username="test"),
+                                                matchdict={'public_language_id': '12345',
+                                                           'public_group_id': '12345',
+                                                           'user_id': '12345',
+                                                           'page_uri': 'http://www.annotran_test.com/'})
+                        result = views.add_report(request)
+                        assert isinstance(result, exc.HTTPNotFound)
 
 
 
+def test_add_existing_report_to_db():
+    """
+        This should not add a new report to the database session as the one already exists.
+    """
+    with mock.patch('annotran.languages.models.Language') as language:
+        language.get_by_public_language_id = MagicMock(return_value=language)
+
+        with mock.patch('annotran.pages.models.Page') as page:
+            page.get_by_uri = MagicMock(return_value=page)
+
+            with mock.patch('h.groups.models.Group') as group:
+                group.get_by_pubid = MagicMock(return_value=group)
+
+                with mock.patch('h.models.User') as user:
+                    user.get_by_username = MagicMock(return_value=user)
+
+                    with mock.patch('annotran.translations.models.Translation') as translation:
+                        translation.get_translation = MagicMock(return_value=translation)
+
+                        with mock.patch('annotran.reports.models.Report') as report:
+                            report.get_report = MagicMock(return_value=report)
+
+                            request = _mock_request(authenticated_user=mock.Mock(username="test"),
+                                                    matchdict={'public_language_id': '12345',
+                                                               'public_group_id': '12345',
+                                                               'user_id': '12345',
+                                                               'page_uri': 'http://www.annotran_test.com/'})
+                            result = views.add_report(request)
+                            assert not request.db.add.called
+                            assert isinstance(result, exc.HTTPBadRequest)
